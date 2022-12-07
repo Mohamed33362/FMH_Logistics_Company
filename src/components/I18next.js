f67a6fd53cdc16e746550c358164297050d77017
mohamed33362 <mohamedessam33362@gmail.com>
2022-12-07 21:31:00 +0200

    something

--- src/components/I18next.js
+++ src/components/I18next.js
@@ -2,16 +2,22 @@ import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import languageDetector from "i18next-browser-languagedetector";
 import HttpApi from "i18next-http-backend";
+import BackendAdapter from "i18next-multiload-backend-adapter";
 
 i18next
   .use(initReactI18next)
   .use(languageDetector)
   .use(HttpApi)
+  .use(BackendAdapter)
   .init({
     supportedLngs: ["en", "ar"],
     backend: {
-      loadPath:"/FMH_Logistics_Company/public/assests/localz/{{lng}}/translation.json",
+      loadPath: "/locales/resources.json?lng={{lng}}&ns={{ns}}",
+      allowMultiLoading: true,
+      addPath: "/locales/add/{{lng}}/{{ns}}",
+      crossDomain: false,
     },
+
     detection: {
       order: ["cookie", "htmlTag", "localStorage", "path", "subdomain"],
       caches: ["cookie"],
